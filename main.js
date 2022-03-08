5f21e13bca762a7a1d03ce4390fbbc768348e112
xVal4k <cristianbricicaru.cb@gmail.com>
2022-03-08 14:52:30 +0100

    Milestone 1 done - js

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const postsLists = document.querySelector(".posts-list");
+
 const posts = [
     {
         "id": 1,
@@ -55,3 +57,41 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+
+
+for (let i = 0; i < posts.length; i++) {
+    
+    let post = document.createElement("div");
+    post.classList.add("post");
+    post.innerHTML = `<div class="post__header">
+        <div class="post-meta">                    
+            <div class="post-meta__icon">
+                <img class="profile-pic" src="${posts[i].author.image}" alt="Phil Mangione">                    
+            </div>
+            <div class="post-meta__data">
+                <div class="post-meta__author">${posts[i].author.name}</div>
+                <div class="post-meta__time">${posts[i].created}</div>
+            </div>                    
+        </div>
+    </div>
+    <div class="post__text">${posts[i].content}</div>
+    <div class="post__image">
+        <img src="${posts[i].media}" alt="">
+    </div>
+    <div class="post__footer">
+        <div class="likes js-likes">
+            <div class="likes__cta">
+                <a class="like-button  js-like-button" href="#" data-postid="1">
+                    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                    <span class="like-button__label">Mi Piace</span>
+                </a>
+            </div>
+            <div class="likes__counter">
+                Piace a <b id="like-counter-1" class="js-likes-counter">${posts[i].likes}</b> persone
+            </div>
+        </div> 
+    </div>`;    
+    postsLists.append(post);
+
+}
